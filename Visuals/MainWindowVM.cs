08df97d4519ac88271a2b95a4b6bbd2bba974ee9
Mitch Graff <Mitch.Graff@cropins.net>
2020-09-21 21:53:43 -0400

    fix file blunder

---
+++ Visuals/MainWindowVM.cs
@@ -0,0 +1,55 @@
+﻿using Models;
+using Prism.Commands;
+using Services;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Visuals
+{
+    public class MainWindowVM : INotifyPropertyChanged
+    {
+        private readonly DelegateCommand _cancelCommand;
+        private readonly ICommand _saveShapeCommand;
+        private readonly IShapeSaver _shapeSaver;
+
+        public ICommand CancelCommand => _cancelCommand;
+        public ICommand SaveShapeCommand => _saveShapeCommand;
+
+
+        public MainWindowVM(IShapeSaver shapeSaver)
+        {
+            _cancelCommand = new DelegateCommand(Cancel);
+            _saveShapeCommand = new DelegateCommand(SaveShape);
+            _shapeSaver = shapeSaver;
+        }
+
+        private void SaveShape()
+        {
+            var shape = new Shape(SelectedShape.Name, SelectedShape.Color, SelectedShape.Size, SelectedShape.X, SelectedShape.Y);
+            _shapeSaver.SaveShape(shape);
+        }
+
+        private void Cancel()
+        {
+            SelectedShape = new ShapeVM();
+        }
+
+        private ShapeVM _selectedShape;
+        public ShapeVM SelectedShape
+        {
+            get { return _selectedShape; }
+            set
+            {
+                _selectedShape = value;
+                OnPropertyChanged(nameof(SelectedShape));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Visuals/ShapeVM.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+
+namespace Visuals
+{
+    public class ShapeVM : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string _color;
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        private double _size;
+        public double Size
+        {
+            get { return _size; }
+            set
+            {
+                _size = value;
+                OnPropertyChanged(nameof(Size));
+            }
+        }
+
+        private int _x;
+        public int X
+        {
+            get { return _x; }
+            set
+            {
+                _x = value;
+                OnPropertyChanged(nameof(X));
+            }
+        }
+
+        private int _y;
+        public int Y
+        {
+            get { return _y; }
+            set
+            {
+                _y = value;
+                OnPropertyChanged(nameof(Y));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
